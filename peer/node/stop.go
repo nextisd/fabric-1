0f5eefb845710e1849bb6873d39590aec3120c87
maffa73 <maffa73@gmail.com>
2016-10-05 21:25:49 +0900

    Update stop.go

--- peer/node/stop.go
+++ peer/node/stop.go
@@ -61,11 +61,15 @@ func stop() (err error) {
 		//fmt.Printf("Stopping local peer using process pid from %s \n", pidFile)
 		logger.Infof("Error trying to connect to local peer: %s", err)
 		logger.Infof("Stopping local peer using process pid from %s", pidFile)
+
+		//@ local peer 의 PID 얻기 ( 어딘가에 /peer.pid 파일 있음 )
 		pid, ferr := readPid(pidFile)
 		if ferr != nil {
 			err = fmt.Errorf("Error trying to read pid from %s: %s", pidFile, ferr)
 			return
 		}
+
+		//@ grpc 연결 실패시, peer 프로세스 종료
 		killerr := syscall.Kill(pid, syscall.SIGTERM)
 		if killerr != nil {
 			err = fmt.Errorf("Error trying to kill -9 pid %d: %s", pid, killerr)
@@ -74,9 +78,24 @@ func stop() (err error) {
 		return nil
 	}
 	logger.Info("Stopping peer using grpc")
+	
+	//@ 별도로 생성하는 resource 는 없는거 같음
+	//@ 인자를 복사해서 adminClient 구조체 만든후 돌려줌
+	//@ 구조체 : adminClient , I/F : AdminClient
 	serverClient := pb.NewAdminClient(clientConn)
 
+	//@ local peer 에게 REST 요청 ("/protos.Admin/StopServer") 보내서 받은 응답을 돌려줌
+	//@ 근데.. 리턴값과 에러 체크도 안 하네??
 	status, err := serverClient.StopServer(context.Background(), &empty.Empty{})
+
+	//@ db 와의 session 종료 ( openchainDB.close() 호출 )
+	//@ 실제 내용은 아래와 같다. ( CF : ColumnFamilyHandle )
+	//@ openchainDB.BlockchainCF.Destroy()
+	//@ openchainDB.StateCF.Destroy()
+	//@ openchainDB.StateDeltaCF.Destroy()
+	//@ openchainDB.IndexesCF.Destroy()
+	//@ openchainDB.PersistCF.Destroy()
+	//@ openchainDB.DB.Close()
 	db.Stop()
 	if err != nil {
 		fmt.Println(&pb.ServerStatus{Status: pb.ServerStatus_STOPPED})
@@ -88,6 +107,9 @@ func stop() (err error) {
 	return err
 }
 
+//@ file 경로를 함수인자로 받아서, 
+//@ 파일을 열고, lock 걸고
+//@ 첨부터 읽어서 숫자로 바꾼뒤 리턴
 func readPid(fileName string) (int, error) {
 	fd, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE, 0644)
 	if err != nil {
