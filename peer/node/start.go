d4c4ef47d1015e8da5232c7d66d8e941d475eb3b
maffa73 <maffa73@gmail.com>
2016-10-06 23:23:08 +0900

    Update start.go

--- peer/node/start.go
+++ peer/node/start.go
@@ -164,16 +164,19 @@ func serve(args []string) error {
 		return secHelper
 	}
 
-	//@@ 
+	//@@ chaincode 를 local peer 에 deploy (build 포함)
+	//@@ ChaincodeSupport 인스턴스 생성하고 gRPC 서버에 등록
+	//@@ system chaincode 를 local peer 에 deploy (build 포함)
 	registerChaincodeSupport(chaincode.DefaultChain, grpcServer, secHelper)
 
 	var peerServer *peer.Impl
 
 	// Create the peerServer
+	//@@ node == validator : genesis block 생성, Engine 가진 peer 인스턴스 생성
 	if peer.ValidatorEnabled() {
 		logger.Debug("Running as validating peer - making genesis block if needed")
 		
-		//@@ 
+		//@@ genesis block 을 생성하고, blockchain 에 insert (once 적용)
 		makeGenesisError := genesis.MakeGenesis()
 		if makeGenesisError != nil {
 			return makeGenesisError
@@ -181,10 +184,22 @@ func serve(args []string) error {
 		logger.Debugf("Running as validating peer - installing consensus %s",
 			viper.GetString("peer.validator.consensus"))
 
-		//@@ 
+		//@@ Ledger 접속 (GetEngine() : 초기화된 peer.Engine 리턴)
+		//@@ Chat service 호출시 새로운 handler 를 생성할 수 있는 (handler factory function) 를 가진 peer 리턴
+		//@@ HandlerFactory : MessageHandlerCoordinator, Stream 를 입력받아 MessageHandler 생성하는 함수
+		//@@ EngineFactory  : MessageHandlerCoordinator 를 입력받아 Engine 생성하는 함수
+		//@@ chatWithSomePeers() 호출 : peerNodes 에 등록된 모든 peer 와 chat session 연결 테스트
+		//@@                           core.yaml "peer.discovery.persist" 에 지정된 file 에 있는 모든 peer 가 대상
 		peerServer, err = peer.NewPeerWithEngine(secHelperFunc, helper.GetEngine)
+
 	} else {
 		logger.Debug("Running as non-validating peer")
+
+		//@@ node != validator
+		//@@ Chat service 호출시 새로운 handler 를 생성할 수 있는 (handler factory function) 를 가진 peer 리턴
+		//@@ validator 와 다른점 : validator 는 engine 생성후 handler factory func. 을 engine 에서 가져왔는데,
+		//@@			  peer 는 입력인자를 그대로 peer 에 세팅 ( chatWithSomePeers() 호출도 동일 )
+		//@@ NewPeerHandler() : Peer handler (HandlerFactory 의 인스턴스) 를 생성, 리턴
 		peerServer, err = peer.NewPeerWithHandler(secHelperFunc, peer.NewPeerHandler)
 	}
 
@@ -195,31 +210,33 @@ func serve(args []string) error {
 	}
 
 	// Register the Peer server
-	//@@ 
+	//@@ peerServer 를 gRPC 서버의 peer service 에 등록
 	pb.RegisterPeerServer(grpcServer, peerServer)
 
 	// Register the Admin server
-	//@@ 
+	//@@ core.NewAdminServer 를 gRPC 서버의 admin service 로 등록
 	pb.RegisterAdminServer(grpcServer, core.NewAdminServer())
 
 	// Register Devops server
-	//@@ 
+	//@@ serverDevops 를 gRPC 서버의 devops service 로 등록
 	serverDevops := core.NewDevopsServer(peerServer)
 	pb.RegisterDevopsServer(grpcServer, serverDevops)
 
 	// Register the ServerOpenchain server
-	//@@ 
+	//@@ Ledger Handler + Peer Info 가진 ServerOpenchain 리턴
+	//@@ NewOpenchainServerWithPeerInfo() : Ledger 에 대한 singleton handler 획득
 	serverOpenchain, err := rest.NewOpenchainServerWithPeerInfo(peerServer)
 	if err != nil {
 		err = fmt.Errorf("Error creating OpenchainServer: %s", err)
 		return err
 	}
 
-	//@@ 
+	//@@ serverOpenchain 를 gRPC 서버의 openchain service 로 등록
 	pb.RegisterOpenchainServer(grpcServer, serverOpenchain)
 
 	// Create and register the REST service if configured
-	//@@ 
+	//@@ REST 서비스 초기화 (core.yaml "rest.enabled" 가 true 일 때)
+	//@@ 서비스 addr : core.yaml "rest.address" ( TLS 적용 : "peer.tls.cert.file" , "peer.tls.key.file" )
 	if viper.GetBool("rest.enabled") {
 		go rest.StartOpenchainRESTServer(serverOpenchain, serverDevops)
 	}
@@ -235,12 +252,13 @@ func serve(args []string) error {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-	//@@ 
+	//@@ ??
+	
 	go func() {
-		sig := <-sigs
+		sig := <-sigs		이건 뭐지? --> signal 받는 channel 생성??
 		fmt.Println()
 		fmt.Println(sig)
-		serve <- nil
+		serve <- nil		serve : error 를 수신하는 channel --> 기다린다는 거야 아닌거야
 	}()
 
 	//@@ 
@@ -254,7 +272,7 @@ func serve(args []string) error {
 		serve <- grpcErr
 	}()
 
-	//@@ 
+	//@@ 지정된 경로의 file 에 자신의 pid 기록
 	if err := writePid(viper.GetString("peer.fileSystemPath")+"/peer.pid", os.Getpid()); err != nil {
 		return err
 	}
@@ -280,18 +298,21 @@ func serve(args []string) error {
 	return <-serve
 }
 
-//@@  
+//@@ registerChaincodeSupport 는 ChaincodeSupport 인스턴스 생성하고 gRPC 서버에 등록
+//@@ system chaincode 를 local peer 에 deploy (build 포함)
 func registerChaincodeSupport(chainname chaincode.ChainName, grpcServer *grpc.Server,
 	secHelper crypto.Peer) {
 
 	//get user mode
+	//@@ core.yaml "chaincode.mode" 가 "dev" 면 userRunsCC = true
 	userRunsCC := false
 	if viper.GetString("chaincode.mode") == chaincode.DevModeUserRunsChaincode {
 		userRunsCC = true
 	}
 
 	//get chaincode startup timeout
-	//@@ 
+	//@@ core.yaml "chaincode.startuptimeout", default = 5sec (5000 msec)
+	//@@ container 를 기동하고 "Register" 수신까지의 timeout
 	tOut, err := strconv.Atoi(viper.GetString("chaincode.startuptimeout"))
 	if err != nil { //what went wrong ?
 		fmt.Printf("could not retrive timeout var...setting to 5secs\n")
@@ -299,15 +320,21 @@ func registerChaincodeSupport(chainname chaincode.ChainName, grpcServer *grpc.Se
 	}
 	ccStartupTimeout := time.Duration(tOut) * time.Millisecond
 
-	//@@ 
+	//@@ ChaincodeSupport 인스턴스 생성
+	//@@ config 설정 loading : core.yaml
+	//@@ 	"peer.networkId" , "peer.id" , "chaincode.installpath" , "peer.tls.enabled"
+	//@@ 	, "peer.tls.cert.file" , "peer.tls.key.file" , "peer.tls.serverhostoverride"
+	//@@ 	, "chaincode.keepalive" , "logging.chaincode"
 	ccSrv := chaincode.NewChaincodeSupport(chainname, peer.GetPeerEndpoint, userRunsCC,
 		ccStartupTimeout, secHelper)
 
 	//Now that chaincode is initialized, register all system chaincodes.
-	//@@ 
+	//@@ fabric 에 등록된 system chaincode 에 대한 hook
+	//@@ note : chaincode 는 여전히 deploy 되어야 함. (user chaincode 와 동일)
+	//@@ register -> build -> deploy to local peer
 	system_chaincode.RegisterSysCCs()
 
-	//@@ 
+	//@@ service 와 그 구현을 gRPC 서버에 등록. 반드시 service invoke 전에 호출되어야 함
 	pb.RegisterChaincodeSupportServer(grpcServer, ccSrv)
 }
 
@@ -330,7 +357,9 @@ func createEventHubServer() (net.Listener, *grpc.Server, error) {
 		//@@ 
 		var opts []grpc.ServerOption
 		if comm.TLSEnabled() {
-			//@@ Server 용 TLS 생성
+			//@@ Server 용 TLS 생성 (TransportAuthenticator)
+			//@@ PEM 으로 인코딩된 file 쌍에서 public/private key 를 parsing
+			//@@ PEM (Privacy Enhanced Mail) : Binary를 Base64로 인코딩
 			creds, err := credentials.NewServerTLSFromFile(
 				viper.GetString("peer.tls.cert.file"),
 				viper.GetString("peer.tls.key.file"))
@@ -341,14 +370,16 @@ func createEventHubServer() (net.Listener, *grpc.Server, error) {
 			opts = []grpc.ServerOption{grpc.Creds(creds)}
 		}
 
-		//@@ 
+		//@@ gRPC 서버 생성 : 등록된 서비스 없으며, 시작되지 않음 (요청처리 불가)
 		grpcServer = grpc.NewServer(opts...)
-		//@@ 
+
+		//@@ 등록 Event : REGISTER / BLOCK / CHAINCODE / REJECTION
+		//@@ go channel 에서 event 기다렸다가, event 별 msg 전송
 		ehServer := producer.NewEventsServer(
 			uint(viper.GetInt("peer.validator.events.buffersize")),
 			viper.GetInt("peer.validator.events.timeout"))
 
-		//@@ 
+		//@@ service 와 그 구현(handler)를 gRPC 서버에 등록. 반드시 invoke 전에 호출되어야 함
 		pb.RegisterEventsServer(grpcServer, ehServer)
 	}
 	return lis, grpcServer, err
