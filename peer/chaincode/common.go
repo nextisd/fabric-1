fbed2473767a638f9afa8ea9c12e7c5519509407
maffa73 <maffa73@gmail.com>
2016-10-11 11:24:16 +0900

    Merge branch 'master' of https://github.com/nextisd/kksl/

--- peer/chaincode/common.go
+++ peer/chaincode/common.go
@@ -33,7 +33,7 @@ import (
 	"golang.org/x/net/context"
 )
 
-// kkkkkk
+// kkkkkkk
 func getChaincodeSpecification(cmd *cobra.Command) (*pb.ChaincodeSpec, error) {
 	spec := &pb.ChaincodeSpec{}
 	if err := checkChaincodeCmdParams(cmd); err != nil {
