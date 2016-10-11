764ef4d4532b092ef141fabc79a480ba9775a7f1
maffa73 <maffa73@gmail.com>
2016-10-11 11:14:20 +0900

    test

--- peer/chaincode/common.go
+++ peer/chaincode/common.go
@@ -33,6 +33,7 @@ import (
 	"golang.org/x/net/context"
 )
 
+// kkk
 func getChaincodeSpecification(cmd *cobra.Command) (*pb.ChaincodeSpec, error) {
 	spec := &pb.ChaincodeSpec{}
 	if err := checkChaincodeCmdParams(cmd); err != nil {
