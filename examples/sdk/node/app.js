0c6b480946edf56390a71750e6a663116933cde9
saemy331 <saeim.son@gmail.com>
2016-10-18 17:24:04 +0900

    examples(SDK)

--- examples/sdk/node/app.js
+++ examples/sdk/node/app.js
@@ -20,13 +20,19 @@
  * 2) User deploys chaincode
  * 3) User queries chaincode
  */
+//@@Node.js를 이용한 간단한 어플리케이션 개발 킷
+//@@사용자를 등록
+//@@일반 체인코드 배포 및 조회 
+
 // "HFC" stands for "Hyperledger Fabric Client"
+//@@ HFC는 Hyperledger Fabric Client를 지칭.
 var hfc = require("hfc");
 
 console.log(" **** starting HFC sample ****");
 
 
 // get the addresses from the docker-compose environment
+//@@docker-compose 환경에서 주소 정보를 얻어옴 : 피어 주소, 멤버쉽 서비스 주소
 var PEER_ADDRESS         = process.env.CORE_PEER_ADDRESS;
 var MEMBERSRVC_ADDRESS   = process.env.MEMBERSRVC_ADDRESS;
 
@@ -34,10 +40,13 @@ var chain, chaincodeID;
 
 // Create a chain object used to interact with the chain.
 // You can name it anything you want as it is only used by client.
+//@@ 체인 객체를 생성하고 이름을 부여.  
 chain = hfc.newChain("mychain");
 // Initialize the place to store sensitive private key information
+//@@ 개인키 정보를 보관할 스토리지를 셋
 chain.setKeyValStore( hfc.newFileKeyValStore('/tmp/keyValStore') );
 // Set the URL to membership services and to the peer
+//@@ 멤버쉽 서비스와 피어에 접근할 URL을 셋
 console.log("member services address ="+MEMBERSRVC_ADDRESS);
 console.log("peer address ="+PEER_ADDRESS);
 chain.setMemberServicesUrl("grpc://"+MEMBERSRVC_ADDRESS);
@@ -45,38 +54,44 @@ chain.addPeer("grpc://"+PEER_ADDRESS);
 
 // The following is required when the peer is started in dev mode
 // (i.e. with the '--peer-chaincodedev' option)
+//@@ 아래는, 피어가 dev(개발) 모드로 구동되었을 때, 요구되는 사항들
 var mode =  process.env['DEPLOY_MODE'];
 console.log("DEPLOY_MODE=" + mode);
 if (mode === 'dev') {
     chain.setDevMode(true);
     //Deploy will not take long as the chain should already be running
-    chain.setDeployWaitTime(10);
+    chain.setDeployWaitTime(10); //체인이 이미 동작하고 있는 상황에서 제한시간 이내(10) 배포가 되어야 함?
 } else {
     chain.setDevMode(false);
-    //Deploy will take much longer in network mode
+    //Deploy will take much longer in network mode // 개발모드가 아닐 경우에는, 배포 제한 시간이 120으로 더 길게 잡힘(네트워킹 고려?)
     chain.setDeployWaitTime(120);
 }
 
 
-chain.setInvokeWaitTime(10);
+chain.setInvokeWaitTime(10); //실행 대기 시간 10, 개발모드는 배포 시간에만 관련 있고 실행에 대한 제한 시간은 동일
 
 // Begin by enrolling the user
+//@@ 사용자를 등록함으로써, 클라이언트가 시작됨.
 enroll();
 
-// Enroll a user.
+// Enroll a user. 
+//@@ 사용자 등록
 function enroll() {
    console.log("enrolling user admin ...");
    // Enroll "admin" which is preregistered in the membersrvc.yaml
-   chain.enroll("admin", "Xurw3yU9zI0l", function(err, admin) {
+   //@@ membersrvc.yaml에 정의된 admin은 미리 등록되어 있음.
+   chain.enroll("admin", "Xurw3yU9zI0l", function(err, admin) { 
       if (err) {
          console.log("ERROR: failed to register admin: %s",err);
          process.exit(1);
       }
       // Set this user as the chain's registrar which is authorized to register other users.
+	  //@@ 해당 유저를 다른 유저의 등록을 허용하는 권한을 가진 체인의 registrar로 등록. 
       chain.setRegistrar(admin);
       
-      var userName = "JohnDoe";
+      var userName = "JohnDoe"; 
       // registrationRequest
+	  //@@ 임의의 사용자 등록 요청을 만듬, 사용자 이름은 JohnDoe
       var registrationRequest = {
           enrollmentID: userName,
           affiliation: "bank_a"
@@ -84,33 +99,41 @@ function enroll() {
       chain.registerAndEnroll(registrationRequest, function(error, user) {
           if (error) throw Error(" Failed to register and enroll " + userName + ": " + error);
           console.log("Enrolled %s successfully\n", userName);
-          deploy(user);
+          deploy(user); //사용자 정보를 deploy
       });      
    });
 }
 
+//@@ client 등록 후(enroll)
+//@@ 모든 함수는 user.으로 실행 user.deploy(request문), user.invoke(request문)... 
 // Deploy chaincode
+//@@ 체인코드 배포
 function deploy(user) {
    console.log("deploying chaincode; please wait ...");
    // Construct the deploy request
+   // 배포 요청을 생성 : 체인코드명, 호출함수, 입력인자들
    var deployRequest = {
        chaincodeName: process.env.CORE_CHAINCODE_ID_NAME,
        fcn: "init",
        args: ["a", "100", "b", "200"]
    };
    // where is the chain code, ignored in dev mode
+   //@@ 배포 대상이되는 체인코드의 경로를 셋
    deployRequest.chaincodePath = "github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02";
 
    // Issue the deploy request and listen for events
+   //@@ 배포 요청을 하고, 이벤트를 수신
    var tx = user.deploy(deployRequest);
    tx.on('complete', function(results) {
        // Deploy request completed successfully
+	   //@@ 배포 요청이 성공적으로 완료 되었을 경우,  
        console.log("deploy complete; results: %j",results);
        // Set the testChaincodeID for subsequent tests
+	   //@@ 체인코드ID를 받음
        chaincodeID = results.chaincodeID;
-       invoke(user);
+       invoke(user); //@@ 배포는 특수한 invoke타입. 
    });
-   tx.on('error', function(error) {
+   tx.on('error', function(error) { //에러 발생시, 종료.
        console.log("Failed to deploy chaincode: request=%j, error=%k",deployRequest,error);
        process.exit(1);
    });
@@ -118,15 +141,18 @@ function deploy(user) {
 }
 
 // Query chaincode
+//@@ 체인코드 쿼리 
 function query(user) {
    console.log("querying chaincode ...");
    // Construct a query request
+   //@@ 쿼리 요청문을 생성 : 체인코드ID, 호출함수query, 함수 아규먼트
    var queryRequest = {
       chaincodeID: chaincodeID,
       fcn: "query",
       args: ["a"]
    };
    // Issue the query request and listen for events
+   //@@ 쿼리 요청을 발행 하고 이벤트를 리슨.
    var tx = user.query(queryRequest);
    tx.on('complete', function (results) {
       console.log("query completed successfully; results=%j",results);
@@ -139,15 +165,18 @@ function query(user) {
 }
 
 //Invoke chaincode
+//@@ 체인코드 실행
 function invoke(user) {
    console.log("invoke chaincode ...");
    // Construct a query request
+   //체인코드 실행 요청문 작성 : 체인코드id, invoke함수호출, 함수 아규먼트
    var invokeRequest = {
       chaincodeID: chaincodeID,
       fcn: "invoke",
       args: ["a", "b", "1"]
    };
    // Issue the invoke request and listen for events
+   //실행 요청을 발행하고, 이벤트를 리슨. 
    var tx = user.invoke(invokeRequest);
    tx.on('submitted', function (results) {
 	      console.log("invoke submitted successfully; results=%j",results);	      
--- examples/sdk/node/web-app.js
+++ examples/sdk/node/web-app.js
@@ -22,6 +22,9 @@
  *    a) register and enroll an identity for the user;
  *    b) use this identity to deploy, query, and invoke a chaincode.
  */
+//@@ web에서 돌아가는 application을 구현하는 방법
+//@@ 1) 초기 : 블록체인에 web app을 enroll (기등록(register)되어 있어야 함)
+//@@ 2) 실행 시점 : web app admin으로 부터 허가된 사용자는 deploy, query, invoke 체인코드를 할 수 있다.
 var hfc = require('hfc');
 
 //get the addresses from the docker-compose environment
@@ -40,6 +43,10 @@ var chain = hfc.newChain("targetChain");
 // either be on a shared file system shared by all members of the cluster
 // or you must implement you own KeyValStore which all members of the
 // cluster can share.
+//@@ FileKeyValStore는 간단한 파일 베이스의 key value 스토어. 
+//@@ 하지만, 사용자가 편의대로 store구성을 바꿀 수 있다. 작업 디렉토리는 클러스터에 있으며,
+//@@ key value store는 클러스터 내의 모든 멤버들에게 공유된 파일 시스템에 있어야 한다.
+//@@ (사용자가 따로 구성한 store도 역시 클러스터 내 모든 멤버들에게 공유가 가능해야 한다.)
 chain.setKeyValStore( hfc.newFileKeyValStore('/tmp/keyValStore') );
 
 // Set the URL for membership services
@@ -47,6 +54,8 @@ chain.setMemberServicesUrl("grpc://MEMBERSRVC_ADDRESS");
 
 // Add at least one peer's URL.  If you add multiple peers, it will failover
 // to the 2nd if the 1st fails, to the 3rd if both the 1st and 2nd fails, etc.
+//@@적어도 하나 이상의 peer와 연결이 되어야 한다.
+//@@여러 피어가 붙을 경우에는, 그 중 하나의 피어와 연결에 문제가 있을 경우, 다른 피어들로 failover된다. 
 chain.addPeer("grpc://PEER_ADDRESS");
 
 // Enroll "WebAppAdmin" which is already registered because it is
@@ -54,17 +63,20 @@ chain.addPeer("grpc://PEER_ADDRESS");
 // If "WebAppAdmin" has already been registered, this will still succeed
 // because it stores the state in the KeyValStore
 // (i.e. in '/tmp/keyValStore' in this sample).
+//@@ WebAppAdmin은 membersrvc.yaml에 정의되어 기등록되어 있다. 
 chain.enroll("WebAppAdmin", "DJY27pEnl16d", function(err, webAppAdmin) {
    if (err) return console.log("ERROR: failed to register %s: %s",err);
    // Successfully enrolled WebAppAdmin during initialization.
    // Set this user as the chain's registrar which is authorized to register other users.
    chain.setRegistrar(webAppAdmin);
    // Now begin listening for web app requests
+   // web app상의 요청 수신 대기
    listenForUserRequests();
 });
 
 // Main web app function to listen for and handle requests.  This is specific to
 // your application but is provided here to demonstrate the pattern.
+// 주요 web app 펑션으로, 요청을 수신 대기하고 핸들링한다. (실제 사용자가 구현할 부분)
 function listenForUserRequests() {
    for (;;) {
       // WebApp-specific logic goes here to await the next request.
@@ -78,11 +90,13 @@ function listenForUserRequests() {
 }
 
 // Handle a user request
+// 위에서 수신한 요청을 실제 핸들링 하는 부분
 function handleUserRequest(userName, userAccount, chaincodeID, fcn, args) {
    // Register and enroll this user.
    // If this user has already been registered and/or enrolled, this will
    // still succeed because the state is kept in the KeyValStore
    // (i.e. in '/tmp/keyValStore' in this sample).
+   // 사용자를 등록하고, 
    var registrationRequest = {
 	         roles: [ 'client' ],
 	         enrollmentID: userName,
@@ -101,16 +115,20 @@ function handleUserRequest(userName, userAccount, chaincodeID, fcn, args) {
         args: args
      };
      // Invoke the request from the user object and wait for events to occur.
+	 // 사용자 객체의 invoke를 실행하고 이벤트를 수신 대기
      var tx = user.invoke(invokeRequest);
      // Listen for the 'submitted' event
+	 // 요청이 제대로 제출되었다는 submitted 메세지를 수신시 정상 처리 
      tx.on('submitted', function(results) {
         console.log("submitted invoke: %j",results);
      });
      // Listen for the 'complete' event.
+	 // 제출한 요청이 정상적으로 실행 완료 되었다는 이벤트 메세지 수신시 정상 처리.
      tx.on('complete', function(results) {
         console.log("completed invoke: %j",results);
      });
      // Listen for the 'error' event.
+	 // 에러 발생시
      tx.on('error', function(err) {
         console.log("error on invoke: %j",err);
      });
